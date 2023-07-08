48dfd60c4cf379cf1903ea026fe9ea0dd9b2ea44
KOBRA-2002 <kobzarev.dani@yandex.ru>
2023-07-08 12:07:05 +0400

    fix

--- Assets/Tests/CreateTestTopologyTest.cs
+++ Assets/Tests/CreateTestTopologyTest.cs
@@ -3,22 +3,8 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using Topology;
 
 public class CreateTestTopologyTest
 {
-    [Test]
-    public void CreateCubeTopology()
-    {
-        DecardGrid cube = DecardTopology.CreateCube(3);
-        int xSize = cube.XSize;
-        int ySize = cube.YSize;
-        int zSize = cube.ZSize;
-        for(int x = 0; x < xSize; x++)
-            for(int y = 0; y < ySize; y++)
-                for(int z = 0; z < zSize; z++)
-                {
-                    Assert.IsTrue(cube[x, y, z].CellState == CellState.isTaken);
-                }
-    }
+
 }
